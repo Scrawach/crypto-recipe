ee2536c7a20e0b0293c62b063c85e6555c3addb3
scrawach <scrawach@gmail.com>
2025-08-08 23:20:45 +0300

    clean: remove unused file from root

--- src/crypto-recipe/auto_export_version_config_file.gd
+++
@@ -1,4 +0,0 @@
-extends "res://addons/AutoExportVersion/VersionProvider.gd"
-
-func get_version(features: PackedStringArray, is_debug: bool, path: String, flags: int) -> String:
-	return "v.%s.%s" % [get_git_commit_count(), get_git_commit_hash()]
