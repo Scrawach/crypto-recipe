eb3c5534a274d202a619953436529087da827f05
scrawach <scrawach@gmail.com>
2025-08-09 01:04:50 +0300

    feat: make naive smooth camera follower

--- src/crypto-recipe/camera/camera_follower.gd
+++ src/crypto-recipe/camera/camera_follower.gd
@@ -2,10 +2,10 @@ class_name CameraFollower
 extends Marker3D
 
 @export var target: Node3D
+@export var speed: float = 10.0
 
 @onready var camera: Camera3D = %Camera3D
 
-
 func _physics_process(delta: float) -> void:
-	# TODO: make smooth movement for camera
-	global_position = target.global_position
+	var direction: Vector3 = target.global_position - global_position
+	global_position += direction * speed * delta
